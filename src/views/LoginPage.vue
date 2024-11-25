4c6a2ddce895c48d3db09f495659d315db744bfb
Maneee <88487704+mymoon69@users.noreply.github.com>
2024-11-25 22:59:05 +0700

    Add files via upload

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class=" h-screen">
+        <!-- background -->
+        <div class="flex bg-cover bg-center h-full justify-center items-center" style="background-image: url(https://media.discordapp.net/attachments/1252175189849018370/1310622431777525771/background.png?ex=6745e3c1&is=67449241&hm=57fa6f2180f0b45226a17f5e5ea0b94268c2aaa1df1fbfea40dbc9d8fca2b13d&=&format=webp&quality=lossless&width=538&height=350);">
+            <!-- login box -->
+            <div class="text-center bg-white w-1/4 rounded-md drop-shadow-lg p-5">
+                <p class="text-2xl leading-relaxed">RESEARCH ADMINISTRATION</p>
+                <p class="text-xl leading-relaxed pb-5">ระบบสนับสนุนงานบริหารงานวิจัย</p>
+                <div class="flex justify-center">
+                    <hr class="w-5/6">
+                </div>
+                <p class="text-lg leading-relaxed pt-5 pb-3">ยืนยันตัวตนด้วยบริการของ Google</p>
+                <!-- button login google -->
+                <button class="btn btn-outline">Sign in with Google</button>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    // export default {
+
+    // }
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
