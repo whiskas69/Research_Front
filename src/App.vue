6a6b5129cfdd0b0786069aff1db8774e17e1efe8
Maneee <88487704+mymoon69@users.noreply.github.com>
2024-11-26 21:00:59 +0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,21 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue";
+import Login from "./views/LoginPage.vue"
+import HomePage_Pro from "./views/HomePage_Profes.vue"
+import HomePage_Ot from "./views/HomePage_Offi.vue";
 </script>
 
 <template>
-  <div> <!-- Root element ห่อส่วนประกอบทั้งหมด -->
-    <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-      <div class="wrapper">
-        <HelloWorld msg="You did it!" />
+  <div>
+    <!-- <Login />/ -->
+     <HomePage_Ot/>
 
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </header>
-
-    <RouterView />
+    <!-- <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button class="btn btn-active btn-ghost">Ghost</button> -->
   </div>
 </template>
 
-<style scoped>
-/* คงสไตล์เดิมไว้ */
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/BoxforChoose.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div class="my-5 py-2 border border-[#D9D9D9] rounded-md text-black hover:cursor-pointer">
+            <p class="flex justify-between px-5 py-1 text-left">
+                <span class="text-base">นายจารุวัฒน์ วรรณวนัช</span>
+                <span class="text-base text-[#868181]">ขออนุมัติเดินทางไปเผยแพร่ผลงานในการประชุมทางวิชาการ</span>
+            </p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
