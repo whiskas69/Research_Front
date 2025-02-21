b46b4c0252ad984741e3570d70a145ae05302f88
mymoon69 <64070105@it.kmitl.ac.th>
2025-02-21 17:20:29 +0700

    i think profile success laew

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,88 +4,137 @@
     <div class="my-5">
       <TextInputLabelLeft
         label="ชื่อ (ภาษาไทย)"
-        customLabel="w-1/6"
+        customLabel="w-4/12"
         customInput="w-full"
-        :placeholder="profileData.nameth"
-        v-model="profileData.nameth"
-        @input="handleInput('nameth', $event.target.value)"
+        :placeholder="user.user_nameth"
+        disabled="true"
       />
     </div>
 
     <div class="my-5">
       <TextInputLabelLeft
         label="ชื่อ (ภาษาอังกฤษ)"
-        customLabel="w-1/6"
+        customLabel="w-4/12"
         customInput="w-full"
-        :placeholder="profileData.nameeng"
-        v-model="profileData.nameeng"
-        @input="handleInput('nameeng', $event.target.value)"
+        :placeholder="user.user_nameeng"
+        disabled="true"
       />
     </div>
 
     <div class="my-5">
       <TextInputLabelLeft
-        label="ตำแหน่ง"
-        customLabel="w-1/6"
+        label="ตำแหน่ง (ภาษาไทย)"
+        customLabel="w-4/12"
         customInput="w-full"
-        :placeholder="profileData.position"
-        v-model="profileData.position"
-        @input="handleInput('position', $event.target.value)"
+        :placeholder="user.user_positionth"
+        disabled="true"
+      />
+    </div>
+
+    <div class="my-5">
+      <TextInputLabelLeft
+        label="ตำแหน่ง (ภาษาอังกฤษ)"
+        customLabel="w-4/12"
+        customInput="w-full"
+        :placeholder="user.user_positioneng"
+        disabled="true"
       />
     </div>
 
     <div class="my-5">
       <TextInputLabelLeft
         label="อีเมล"
-        customLabel="w-1/6"
+        customLabel="w-4/12"
         customInput="w-full"
-        :placeholder="profileData.email"
-        v-model="profileData.email"
+        :placeholder="user.user_email"
         disabled="true"
       />
     </div>
 
     <div class="my-5">
       <TextInputLabelLeft
-        label="ยอดคงเหลือของการขออนุมัติเดินทาง ไปเผยแพร่ผลงานในการประชุมวิชาการ"
+        label="ยอดคงเหลือของการขออนุมัติเดินทางไปเผยแพร่ผลงานในการประชุมวิชาการ"
         customLabel="w-full max-w-fit mr-5"
         customInput="w-full"
-        :placeholder="profileData.money"
-        v-model="profileData.money"
+        :placeholder="user.user_moneyCF"
         disabled="true"
       />
     </div>
 
-    <button @click="updatedProfile" class="btn bg-blue-500 text-white">
-      ตกลง
-    </button>
+    <div class="my-5">
+      <TextInputLabelLeft
+        label="ยอดที่ใช้ของการขออนุมัติค่า Page Charge เพื่อตีพิมพ์ผลงานในวารสารวิชาการระดับนานาชาติ"
+        customLabel="w-full max-w-fit mr-5"
+        customInput="w-full"
+        :placeholder="user.user_moneyPC"
+        disabled="true"
+      />
+    </div>
 
     <div class="my-10 flex flex-col">
       <h1 class="text-xl font-bold">ลายเซ็น</h1>
-      <p class="text-red-600">
-        สามารถอัปโหลดลายเซ็นได้เพียง 1 ครั้งเท่านั้น และสกุลไฟล์เป็น png
-        กรุณาตรวจสอบความถูกต้องก่อนกดยืนยัน
-      </p>
 
-      <div class="my-5">
-        <input
-          type="file"
-          class="file-input file-input-bordered w-full max-w-xs"
-          @change="handleFile($event, 'signature')"
-        />
+      <div v-if="user.user_signature == null || user.user_signature == ''">
+        <p class="text-red-600">
+          สามารถอัปโหลดลายเซ็นได้เพียง 1 ครั้งเท่านั้น และสกุลไฟล์เป็น png
+          กรุณาตรวจสอบความถูกต้องก่อนกดยืนยัน
+        </p>
+
+        <div class="my-5">
+          <input
+            type="file"
+            class="file-input file-input-bordered w-full max-w-xs"
+            @change="handleFile($event, 'signature')"
+          />
+        </div>
+
+        <span v-if="v$.signature.$error" class="text-base ml-2 text-red-500">
+          {{ v$.signature.$errors[0].$message }}
+        </span>
+
+        <div class="flex justify-end mr-5">
+          <button @click="updatesignature" class="btn bg-blue-500 text-white">
+            ตกลง
+          </button>
+        </div>
       </div>
 
-      <div>
-        <button @click="updatesignature" class="btn bg-blue-500 text-white">
-          ตกลง
-        </button>
+      <div v-else>
+        <p class="text-red-600">
+          หากมีข้อผิดพลาดโปรดติดต่อเจ้าหน้าที่ที่เกี่ยวข้อง เพื่อแก้ไข
+        </p>
+        <div class="flex flex-row items-center w-full">
+          <div class="flex flex-row items-center w-full">
+            <div class="flex flex-row">
+              <p>ดูลายเซ็นที่อัปโหลด</p>
+            </div>
+            <div class="">
+              <button
+                @click="getSignature"
+                class="btn bg-[#E85F19] text-white ml-10"
+              >
+                ดูเอกสาร
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <img
+          v-if="data.signature"
+          :src="data.signature"
+          alt="signature"
+          class="w-64 h-64 mt-5"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted, reactive } from "vue";
+import { computed, onMounted, reactive } from "vue";
+
+import { useVuelidate } from "@vuelidate/core";
+import { helpers, required } from "@vuelidate/validators";
 
 import { useUserStore } from "@/store/userStore";
 import TextInputLabelLeft from "@/components/Input/TextInputLabelLeft.vue";
@@ -93,90 +142,82 @@ import api from "@/setting/api";
 
 const userStore = useUserStore();
 
-const profileData = reactive({
-  Id: "",
-  nameth: "",
-  nameeng: "",
-  position: "",
-  email: "",
-  money: "",
+const user = computed(() => userStore.user);
+
+const data = reactive({
+  id: "",
   signature: null,
 });
 
-const user = computed(() => userStore.user);
+//validate rule
+const rules = {
+  signature: {
+    required: helpers.withMessage("* กรุณาอัปโหลดไฟล์ *", required),
+    fileType: helpers.withMessage(
+      "* อัปโหลดได้เฉพาะไฟล์ png เท่านั้น *",
+      (value) => {
+        if (!value) return false;
+        const allowedTypes = ["image/png"];
+        return allowedTypes.includes(value.type);
+      }
+    ),
+  },
+};
+
+const v$ = useVuelidate(rules, data);
 
 onMounted(async () => {
   await userStore.fetchUser();
-  console.log("user : ", user.value?.user_id);
 
-  // อัปเดตค่าหลังจาก `user` โหลดเสร็จ
-  profileData.Id = user.value?.user_id;
-  profileData.nameth = user.value?.user_nameth || "";
-  profileData.nameeng = user.value?.user_nameeng || "";
-  profileData.position = user.value?.user_position || "";
-  profileData.email = user.value?.user_email || "";
-  profileData.money = user.value?.user_money || "";
-  profileData.signature = user.value?.user_signature || "";
+  data.id = user.value?.user_id;
 });
-
-const handleInput = (key, value) => {
-  profileData[key] = value;
-};
 
 const handleFile = (event, fieldName) => {
   const signature = event.target.files[0];
   if (signature) {
-    profileData[fieldName] = signature;
-    console.log(`File assigned to ${fieldName}:`, profileData[fieldName]);
-    console.log("Updated profileData:", profileData);
+    data[fieldName] = signature;
+    console.log(`File assigned to ${fieldName}:`, data[fieldName]);
+    console.log("Updated data:", data);
   } else {
     console.error(`No file selected for ${fieldName}.`);
   }
 };
 
-const updatedProfile = async () => {
-  try {
-    const DataforUpdate = {
-      user_id: profileData.Id,
-      user_nameth: profileData.nameth,
-      user_nameeng: profileData.nameeng,
-      user_position: profileData.position,
-      user_email: profileData.email,
-      user_money: profileData.money,
-    };
+const updatesignature = async () => {
+  const result = await v$.value.$validate();
 
-    console.log("DataforUpdate : ", DataforUpdate);
-    const response = await api.put("/profileupdate", DataforUpdate);
+  if (result) {
+    try {
+      const DataforUpdate = {
+        user_id: data.id,
+        user_signature: data.signature,
+      };
 
-    alert("Update Success");
-    console.log("response : ", response);
+      console.log("DataforUpdate : ", DataforUpdate);
 
-    location.reload();
-  } catch (error) {
-    console.log("error", error);
+      const response = await api.put("/uploadSignature", DataforUpdate, {
+        headers: {
+          "Content-Type": "multipart/form-data", // Required for file uploads
+        },
+      });
+
+      alert("อัปโหลดลายเซ็นเรียบร้อยแล้ว");
+
+      location.reload();
+    } catch (error) {
+      console.log("error", error);
+    }
+  } else {
+    alert("โปรดกรอกข้อมูลให้ครบถ้วน");
   }
 };
 
-const updatesignature = async () => {
-  try {
-    const DataforUpdate = {
-      user_id: profileData.Id,
-      user_signature: profileData.signature,
-    };
+const getSignature = async () => {
+  const user_id = data.id;
 
-    console.log("DataforUpdate : ", DataforUpdate);
-    const response = await api.put("/signatureupdate", DataforUpdate, {
-      headers: {
-        "Content-Type": "multipart/form-data", // Required for file uploads
-      },
-    });
+  const res = await api.get(`/mySignature?user_id=${user_id}`);
 
-    alert("Update Success");
-    console.log("response : ", response);
-
-    location.reload();
-  } catch (error) {
-    console.log("error", error);
-  }
+  console.log(res);
+  data.signature = res.data.fileUrl;
 };
 </script>
