01a78ecf4773d1c717fd89734eb21f58f42595fc
whiskas69 <64070075@it.kmail.ac.th>
2025-03-11 23:32:19 +0700

    add user

--- src/views/Homepage/Homepage_Admin.vue
+++ src/views/Homepage/Homepage_Admin.vue
@@ -3,9 +3,82 @@
     <div class="container my-10 mx-auto">
       <p class="text-xl font-bold pb-5">การจัดการผู้ใช้</p>
       <p class="text-xl font-bold pb-5">มีการเพิ่มผุใช้ด้วย</p>
+      <TextInputLabelLeft
+        label="ชื่อ (ภาษาไทย)"
+        customLabel="mr-2"
+        customInput="max-w-max"
+        customDiv="max-w-max"
+        @input="handleInputUser('nameth', $event)"
+      />
+      <TextInputLabelLeft
+        label="ชื่อ (ภาษาอังกฤษ)"
+        customLabel="mr-2"
+        customInput="max-w-max"
+        customDiv="max-w-max"
+        @input="handleInputUser('nameeng', $event)"
+      />
+      <select
+        class="select select-bordered flex-1"
+        @change="handleInputUser('role', $event)"
+      >
+        <option disabled selected>เลือกหน้าที่ผู้ใช้</option>
+        <option :value="'professor'">อาจารย์</option>
+        <option :value="'hr'">เจ้าหน้าที่บริหารทรัพยากรบุคคล</option>
+        <option :value="'research'">เจ้าหน้าที่งานวิจัย</option>
+        <option :value="'finance'">เจ้าหน้าที่การเงิน</option>
+        <option :value="'associate'">รองคณบดี</option>
+        <option :value="'dean'">คณบดี</option>
+        <option :value="'admin'">ผู้ดูแล</option>
+      </select>
+      <TextInputLabelLeft
+        label="อีเมล"
+        customLabel="mr-2"
+        customInput="max-w-max"
+        customDiv="max-w-max"
+        @input="handleInputUser('email', $event)"
+      />
+      <TextInputLabelLeft
+        label="ตำแหน่ง (ภาษาไทย)"
+        customLabel="mr-2"
+        customInput="max-w-max"
+        customDiv="max-w-max"
+        @input="handleInputUser('positionth', $event)"
+      />
+      <TextInputLabelLeft
+        label="ตำแหน่ง (ภาษาอังกฤษ)"
+        customLabel="mr-2"
+        customInput="max-w-max"
+        customDiv="max-w-max"
+        @input="handleInputUser('positioneng', $event)"
+      />
+      <TextInputLabelLeft
+        label="วันที่เริ่มทำงาน"
+        type="date"
+        customLabel="mr-2"
+        customInput="max-w-max"
+        customDiv="max-w-max"
+        @input="handleInputUser('startwork', $event)"
+      />
+      <TextInputLabelLeft
+        label="จำนวนปีที่เริ่มทำงาน"
+        customLabel="mr-2"
+        customInput="max-w-max"
+        customDiv="max-w-max"
+        @input="handleInputUser('year', $event)"
+      />
+      <TextInputLabelLeft
+        label="ยอดเงินการขออนุมัติเดินทางไปเผยแพร่ผลงานในการประชุมวิชาการ"
+        customLabel="mr-2"
+        customInput="max-w-max"
+        customDiv="max-w-max"
+        @input="handleInputUser('moneyCF', $event)"
+      />
+      <button @click="addUser" class="btn btn-error text-base text-white">
+        เพิ่มผู้ใช้
+      </button>
       <!-- ปุ่มแก้ไข -->
       <div class="flex justify-end mt-5">
-        <button @click="openEdit" class="btn btn-success text-white">
+        <button @click="openEdit" class="btn btn-success text-base text-white">
           แก้ไข
         </button>
       </div>
@@ -74,7 +147,7 @@
                   </td>
                   <td class="text-center">
                     <TextInputLabelLeft
-                    v-if="user.user_role == 'professor'"
+                      v-if="user.user_role == 'professor'"
                       customLabel="w-auto min-w-fit"
                       customDiv="max-w-max mr-10"
                       :placeholder="user.user_moneyCF"
@@ -82,7 +155,10 @@
                     />
                   </td>
                   <td class="text-center">
-                    <button @click="deleteUser(user.user_id)" class="btn btn-error text-base text-white">
+                    <button
+                      @click="deleteUser(user.user_id)"
+                      class="btn btn-error text-base text-white"
+                    >
                       ลบผู้ใช้
                     </button>
                   </td>
@@ -120,7 +196,9 @@
               <th class="text-center">{{ index + 1 }}</th>
               <td class="text-center">{{ user.user_nameth }}</td>
               <td class="text-center">{{ user.user_role }}</td>
-              <td class="text-center" v-if="user.user_role == 'professor'">{{ user.user_moneyCF }}</td>
+              <td class="text-center" v-if="user.user_role == 'professor'">
+                {{ user.user_moneyCF }}
+              </td>
             </tr>
           </tbody>
         </table>
@@ -132,6 +210,7 @@
 <script setup>
 import { ref, reactive, computed, onMounted } from "vue";
 import axios from "axios";
+import api from "@/setting/api";
 import TextInputLabelLeft from "@/components/Input/TextInputLabelLeft.vue";
 
 const showEdit = ref(false);
@@ -151,12 +230,45 @@ const closeEdit = () => {
 const formData = reactive({
   users: [],
   userRoles: [],
-  userMoneyCF: []
+  userMoneyCF: [],
+
+  role: "",
+  nameth: "",
+  nameeng: "",
+  email: "",
+  moneyCF: "",
+  positionth: "",
+  positioneng: "",
+  startwork: "",
+  year: "",
 });
+const inputTypes = {
+  role: "string",
+  nameth: "string",
+  nameeng: "string",
+  email: "string",
+  moneyCF: "number",
+  positionth: "string",
+  positioneng: "string",
+  startwork: "date",
+  year: "number",
+};
+const handleInputUser = (key, event) => {
+  const value = event.target.value.trim();
+  const type = inputTypes[key];
+  if (type === "number") {
+    formData[key] = value === "" ? null : parseInt(value, 10);
+  } else {
+    formData[key] = value;
+  }
+  console.log(`Updated formData ${key} is ${value}:`);
+  // JSON.stringify(formData)
+};
 
 const handleInput = (id, event, type) => {
   let value = event.target.value.trim(); // ตัดช่องว่าง
-  let targetArray = type === "money" ? formData.userMoneyCF : formData.userRoles;
+  let targetArray =
+    type === "money" ? formData.userMoneyCF : formData.userRoles;
 
   // แปลงค่าเป็นตัวเลขถ้าเป็น `money`
   if (type === "money") {
@@ -164,7 +276,7 @@ const handleInput = (id, event, type) => {
   }
   // ตรวจสอบว่ามี `id` นี้อยู่แล้วหรือไม่
   const index = targetArray.findIndex((item) => item.id === id);
-  console.log("index", index)
+  console.log("index", index);
   if (index !== -1) {
     // ถ้ามีแล้วให้อัปเดตค่าใหม่
     targetArray[index].value = value;
@@ -192,18 +304,46 @@ const fetchOfficerData = async () => {
 const updateUserRoles = async () => {
   try {
     const mergedData = [...formData.userRoles, ...formData.userMoneyCF];
-    console.log("mergedData", mergedData)
+    console.log("mergedData", mergedData);
     const response = await axios.put("http://localhost:3000/updateRoles", {
       userUpdates: mergedData, // ส่ง userRoles [{ id: 1, value: 'admin' }, ...]
     });
 
     console.log("Response from server:", response.data);
-    
+
     alert("บันทึกข้อมูลเรียบร้อยแล้ว");
     location.reload();
   } catch (error) {
     console.error("Error updating user roles:", error);
     alert("เกิดข้อผิดพลาดในการอัปเดตข้อมูล");
+  }
+};
+const addUser = async () => {
+  try {
+    const dataForBackend = {
+      user_role: formData.role,
+      user_nameth: formData.nameth,
+      user_nameeng: formData.nameeng,
+      user_email: formData.email,
+      user_moneyCF: formData.moneyCF,
+      user_positionth: formData.positionth,
+      user_positioneng: formData.positioneng,
+      user_startwork: formData.startwork,
+      user_year: formData.year,
+    };
+    console.log("post user: ", JSON.stringify(dataForBackend));
+    const response = await api.post("/user", dataForBackend, {
+      headers: {
+     "Content-Type": "application/json",
+  },
+    });
+    console.log("post response: ", response);
+    alert("บันทึกข้อมูลเรียบร้อยแล้ว");
+    
+  } catch (error) {
+    console.log("Error saving code : ", error);
+
+    alert("ไม่สามารถส่งข้อมูล โปรดลองอีกครั้งในภายหลัง");
   }
 };
 
@@ -214,14 +354,14 @@ const deleteUser = async (id) => {
     });
 
     console.log("delete id:", response.data);
-    
+
     alert("ลบผู้ใช้เรียบร้อย");
     window.location.reload();
   } catch (error) {
     console.error("Error updating user roles:", error);
     alert("เกิดข้อผิดพลาดในการอัปเดตข้อมูล");
   }
-}
+};
 
 onMounted(async () => {
   await fetchOfficerData();
