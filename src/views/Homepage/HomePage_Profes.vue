af136f7fa51a34bc6cc9a25576e3c6ae72dec30d
whiskas69 <64070075@it.kmail.ac.th>
2025-05-24 22:22:09 +0700

    edit form v.2

--- src/views/Homepage/HomePage_Profes.vue
+++ src/views/Homepage/HomePage_Profes.vue
@@ -4,8 +4,6 @@
     <div class="container my-10 mx-auto">
       <p class="text-xl font-bold pb-5">เอกสารที่ต้องการยื่น</p>
       <!-- box first -->
-      <!-- {{ userStore.user }} -->
-      <!-- {{ data.approvedConfer }} -->
       <div
         class="my-5 py-2 border border-[#D9D9D9] rounded-md text-black hover:cursor-pointer"
         :class="{
@@ -43,6 +41,77 @@
         </router-link>
       </div>
     </div>
+    <div class="container my-10 mx-auto">
+      <p class="text-xl font-bold pb-5">เอกสารที่ได้รับการแก้ไข</p>
+      {{ data.editForm }}
+      <div v-for="editForm in data.editForm" :key="editForm.form_id">
+        <div
+          class="p-5 shadow m-5 rounded-xl hover:cursor-pointer mb-2"
+          v-if="editForm.form_type == 'Research_KRIS'"
+        >
+          <router-link :to="`/modifiedKris/${editForm.kris_id}`">
+            <h2 class="text-lg font-bold">
+              แบบเสนอโครงการวิจัย ทุนวิจัยส่งเสริมส่วนงานวิชาการ
+            </h2>
+            <div class="mt-2 ml-5">
+              <div>
+                <div class="flex">
+                  <h4 class="mr-5">
+                    ชื่อโครงการวิจัย : {{ editForm.article_title }}
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </router-link>
+        </div>
+
+        <div
+          class="p-5 shadow m-5 rounded-xl hover:cursor-pointer mb-2"
+          v-if="editForm.form_type == 'Page_Charge'"
+        >
+          <router-link :to="`/modifiedPage/${editForm.pageC_id}`">
+            <h2 class="text-lg font-bold">
+              ขออนุมัติค่า Page
+              Chargeเพื่อตีพิมพ์ผลงานในวารสารวิชาการระดับนานาชาติ
+            </h2>
+            <div class="mt-2 ml-5">
+              <div>
+                <div class="flex">
+                  <h4 class="mr-5">ชื่อวารสาร : {{ editForm.article_name }}</h4>
+                </div>
+                <div class="flex">
+                  <h4 class="mr-5">ชื่อบทความ : {{ editForm.article_title }}</h4>
+                </div>
+                
+              </div>
+            </div>
+          </router-link>
+        </div>
+
+        <div
+          class="p-5 shadow m-5 rounded-xl hover:cursor-pointer mb-2"
+          v-if="editForm.form_type == 'Conference'"
+        >
+          <router-link :to="`/modifiedConfer/${editForm.conf_id}`">
+            <h2 class="text-lg font-bold">
+              ขออนุมัติเดินทางไปเผยแพร่ผลงานในการประชุมทางวิชาการ
+            </h2>
+            <div class="mt-2 ml-5">
+              <div>
+                <div class="flex">
+                  <h4 class="mr-5">ชื่องานประชุม : {{ editForm.article_name }}</h4>
+                </div>
+                <div class="flex">
+                  <h4 class="mr-5">ชื่อบทความ : {{ editForm.article_title }}</h4>
+                </div>
+              </div>
+            </div>
+          </router-link>
+        </div>
+        
+      </div>
+    </div>
+    
   </div>
 </template>
 
@@ -55,6 +124,7 @@ import api from "@/setting/api";
 const data = reactive({
   id: "",
   approvedConfer: 0,
+  editForm: [],
 });
 
 const router = useRouter();
@@ -67,12 +137,23 @@ if (!userStore.user.user_signature) {
   router.push("/profile");
 }
 
-const getDataMoney = async () => {
+const getData = async () => {
   console.log("id", data.id);
   const res = await api.get(`/sumBudgets/${user.value?.user_id}`);
   console.log("res", res.data);
   data.approvedConfer = res.data.sumConfer.length;
   console.log("data.approvedConfer", data.approvedConfer);
+  const resEdit = await api.get(`/form/${user.value?.user_id}`)
+  console.log("data", resEdit.data);
+  for(let i = 0; i < resEdit.data.length; i++){
+    console.log("have edit ja", i)
+    if(resEdit.data[i].edit_data != null){
+      console.log("have edit ja", resEdit.data[i])
+      data.editForm.push(resEdit.data[i])
+    }
+  }
+  console.log("wow za", data.editForm)
+
 };
 
 onMounted(async () => {
@@ -81,6 +162,6 @@ onMounted(async () => {
   }
   await userStore.fetchUser();
   data.id = user.value?.user_id;
-  await getDataMoney();
+  await getData();
 });
 </script>
