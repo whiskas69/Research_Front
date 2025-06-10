fdb98889fa1de213552426aa465c85af595b1855
whiskas69 <64070075@it.kmail.ac.th>
2025-06-10 23:53:21 +0700

    credentials api

--- src/setting/api.js
+++ src/setting/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: "http://localhost:3002", //backend url
     withCredentials: true, //send cookie
+    credentials: 'include',
 });
 
 export default api;
